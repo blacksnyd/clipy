9b0b9e2a2994c2dfb271416feed4295647349840
Kenzo douchet <kenzodouchet33@gmail.com>
2025-12-15 16:53:50 +0100

    endpoint all videos

--- back_end/src/controllers/videos_controller.js
+++ back_end/src/controllers/videos_controller.js
@@ -1,5 +1,20 @@
+import db from '../config/db_pool.js';
+
 export const all = async (req,res) => {
-  console.log("test all");
+  try {
+    const [rows] = await db.pool.execute('SELECT * FROM videos');
+
+    res.status(200).json({
+      success: true,
+      data: rows
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: 'Erreur lors de la récupération des catégories'
+    });
+  }
 }
 export const create = async (req,res) => {
   console.log("test post");
