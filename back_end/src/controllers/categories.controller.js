1ead47a4f55ef8348855710526c13007bf62473c
Kenzo douchet <kenzodouchet33@gmail.com>
2025-12-15 16:19:45 +0100

    endpoint all categories

--- back_end/src/controllers/categories.controller.js
+++ back_end/src/controllers/categories.controller.js
@@ -1,3 +1,19 @@
-export const all = (req, res) => {
-  console.log('test all');
+import db from '../config/db_pool.js';
+
+export const all = async (req, res) => {
+ try {
+  const [rows] = await db.pool.execute(
+    'SELECT * FROM categories'
+  );
+  res.status(200).json({
+    success: true,
+    data: rows
+  });
+} catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: 'Erreur lors de la récupération des catégories'
+    });
+  }
 };
