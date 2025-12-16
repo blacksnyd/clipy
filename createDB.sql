92c7e1ea054cee78a541933e1157ca769f5bb7d7
Kenzo douchet <kenzodouchet33@gmail.com>
2025-12-16 16:01:00 +0100

    add column cover to videos

--- createDB.sql
+++ createDB.sql
@@ -9,6 +9,7 @@ CREATE TABLE videos (
    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
    title varchar(150) NOT NULL,
    URL varchar(500) NOT NULL,
+   cover varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
    duration int NOT NULL,
    description text NOT NULL,
    category_id int,
@@ -20,7 +21,7 @@ CREATE TABLE ratings (
    value int,
    video_id int,
    FOREIGN KEY (video_id) REFERENCES videos(id)
-); 
+);
 
 CREATE TABLE comments (
    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
