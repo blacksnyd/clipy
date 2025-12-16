4bf597a60f0599238525b3f6547edd8179edf560
MathCT <mathieu.chatrain@gmail.com>
2025-12-16 10:52:04 +0100

    commit

--- back_end/src/routes/videos_routes.js
+++ back_end/src/routes/videos_routes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 
-import {all, show, create, deleteVideo, updateVideo} from '../controllers/videos_controller.js';
+import {all, show, deleteVideo, updateVideo} from '../controllers/videos_controller.js';
 
 const router = Router();
 
