cc48bbaed2e47610703942bb808d4e1e2a55cc67
ThibFr33 <thibs.cha@gmail.com>
2025-12-17 16:04:07 +0100

    db-updated again

--- createDB.sql
+++ createDB.sql
@@ -1,5 +1,11 @@
 CREATE DATABASE clipy_db;
 
+CREATE TABLE users (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  username varchar(100) NOT NULL UNIQUE,
+  email varchar(150) NOT NULL UNIQUE,
+  password_hash varchar(255) NOT NULL
+);
 CREATE TABLE categories (
    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
    title varchar(100)
@@ -13,19 +19,25 @@ CREATE TABLE videos (
    duration int NOT NULL,
    description text NOT NULL,
    category_id int,
-   FOREIGN KEY (category_id) REFERENCES categories(id)
+   user_id INT NOT NULL,
+   FOREIGN KEY (category_id) REFERENCES categories(id),
+   FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE TABLE ratings (
    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
    value int,
    video_id int,
-   FOREIGN KEY (video_id) REFERENCES videos(id)
+   user_id INT NOT NULL,
+   FOREIGN KEY (video_id) REFERENCES videos(id),
+   FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE TABLE comments (
    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
    content text,
    video_id int,
-   FOREIGN KEY (video_id) REFERENCES videos(id)
+   user_id INT NOT NULL,
+   FOREIGN KEY (video_id) REFERENCES videos(id),
+   FOREIGN KEY (user_id) REFERENCES users(id)
 );
