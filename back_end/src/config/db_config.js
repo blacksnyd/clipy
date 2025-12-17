2abefda52d254476bd39bb5d3d64a2c3c90b608e
Victor-Voyer <victor.voyer@oclock.school>
2025-12-17 16:32:47 +0100

    update process PORT --> JWT_TOKEN

--- back_end/src/config/db_config.js
+++ back_end/src/config/db_config.js
@@ -24,5 +24,5 @@ export default {
    DB_USER : process.env.DB_USER,
    DB_PASSWORD : process.env.DB_PASSWORD,
    DB_PORT : process.env.DB_PORT,
-   JWT_SECRET: process.env.DB_PORT
+   JWT_SECRET: process.env.JWT_SECRET,
 }
