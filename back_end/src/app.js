56061a11ca7aac1fe310b219d5a97b6fbcd1fa5d
Kenzo douchet <kenzodouchet33@gmail.com>
2025-12-18 10:41:06 +0100

    add pagination in findall service video

--- back_end/src/app.js
+++ back_end/src/app.js
@@ -4,6 +4,8 @@ import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import routes from './routes/index.js';
+import paginate from 'express-paginate';
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -13,10 +15,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(paginate.middleware(10, 50))
+
+
 
 // Servir les fichiers statiques depuis le dossier uploads
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
 app.use('/api', routes);
+
+
 
 export default app;
