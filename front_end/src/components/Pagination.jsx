eb29ef49e0dc8dff1f54f40b077b5d8703f9b26f
Kenzo douchet <kenzodouchet33@gmail.com>
2025-12-18 11:58:14 +0100

    add props and for boucle in pagination jsx

--- front_end/src/components/Pagination.jsx
+++ front_end/src/components/Pagination.jsx
@@ -1,8 +1,26 @@
-function Pagination() {
-  return(
-    <>
-      <h1>test</h1>
-    </>
+// src/components/Pagination.jsx
+import React from 'react'
+
+function Pagination({ currentPage, totalPages, onPageChange }) {
+  const pages = []
+
+  for (let i = 1; i <= totalPages; i++) {
+    pages.push(
+      <button
+        key={i}
+        className={`join-item btn ${i === currentPage ? 'btn-primary' : ''}`}
+        onClick={() => onPageChange(i)}
+      >
+        {i}
+      </button>
+    )
+  }
+
+  return (
+    <div className="join mt-4 justify-center">
+      {pages}
+    </div>
   )
 }
+
 export default Pagination
